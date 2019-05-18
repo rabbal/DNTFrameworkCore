1550d0842343cc36e0b2565d56092d8ffa449dbf
GholamReza Rabbal <gholamrezarabbal@gmail.com>
2019-05-19 01:45:43 +0430

    fixed build issue

--- src/DNTFrameworkCore.Cqrs/Behaviors/ValidatorBehavior.cs
+++ src/DNTFrameworkCore.Cqrs/Behaviors/ValidatorBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using DNTFrameworkCore.Cqrs.Commands;
 using DNTFrameworkCore.Functional;
@@ -13,37 +14,39 @@ namespace DNTFrameworkCore.Cqrs.Behaviors
     {
         public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
         {
-      private readonly ILogger<ValidatorBehavior<TRequest, TResponse>> _logger;
-        private readonly IValidator<TRequest>[] _validators;
+            //private readonly ILogger<ValidatorBehavior<TRequest, TResponse>> _logger;
+            //  private readonly IValidator<TRequest>[] _validators;
 
-        public ValidatorBehavior(IValidator<TRequest>[] validators, ILogger<ValidatorBehavior<TRequest, TResponse>> logger)
-        {
-            _validators = validators;
-            _logger = logger;
-        }
+            //  public ValidatorBehavior(IValidator<TRequest>[] validators, ILogger<ValidatorBehavior<TRequest, TResponse>> logger)
+            //  {
+            //      _validators = validators;
+            //      _logger = logger;
+            //  }
 
-        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
-        {
-            var typeName = request.GetGenericTypeName();
+            //  public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+            //  {
+            //      var typeName = request.GetGenericTypeName();
 
-            _logger.LogInformation("----- Validating command {CommandType}", typeName);
+            //      _logger.LogInformation("----- Validating command {CommandType}", typeName);
 
-            var failures = _validators
-                .Select(v => v.Validate(request))
-                .SelectMany(result => result.Errors)
-                .Where(error => error != null)
-                .ToList();
+            //      var failures = _validators
+            //          .Select(v => v.Validate(request))
+            //          .SelectMany(result => result.Errors)
+            //          .Where(error => error != null)
+            //          .ToList();
 
-            if (failures.Any())
-            {
-                _logger.LogWarning("Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}", typeName, request, failures);
+            //      if (failures.Any())
+            //      {
+            //          _logger.LogWarning("Validation errors - {CommandType} - Command: {@Command} - Errors: {@ValidationErrors}", typeName, request, failures);
 
-                throw new OrderingDomainException(
-                    $"Command Validation Errors for type {typeof(TRequest).Name}", new ValidationException("Validation exception", failures));
-            }
+            //          throw new OrderingDomainException(
+            //              $"Command Validation Errors for type {typeof(TRequest).Name}", new ValidationException("Validation exception", failures));
+            //      }
 
-            return await next();
+            //      return await next();
+
+            throw new NotImplementedException();
         }
     }
 }
-}
+
